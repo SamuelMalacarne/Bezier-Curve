ab7188f2f546cc6601808d197dceb636e86e4d77
Samuel <samuelmalacarne.sm@gmail.com>
2022-10-29 13:28:55 +0200

    Added a menu, but still has some bugs

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ screen = pygame.display.set_mode((WIDTH, HEIGHT))
 clock = pygame.time.Clock()
 SCREEN_UPDATE = pygame.USEREVENT
 pygame.time.set_timer(SCREEN_UPDATE, 10)
+font = pygame.font.SysFont('arial', 30)
 
 
 def lerp(p0, p1, t):
@@ -30,6 +31,12 @@ def create_dot_rect_surface(center, size = 5):
 
 class Curve():
     def __init__(self):
+        self.p0 = Vector2(0, HEIGHT/2)
+        self.p1 = Vector2(WIDTH, HEIGHT/2)
+        self.t = 0
+        self.bezier = list()
+
+    def reset(self):
         self.p0 = Vector2(0, HEIGHT/2)
         self.p1 = Vector2(WIDTH, HEIGHT/2)
         self.t = 0
@@ -87,36 +94,148 @@ class Curve():
             self.bezier.append(Vector2(x, y))
 
 
-    def render_bezier(self):
-        prev_point = self.p0
+    def render_bezier(self, str_p):
+        prev_point = str_p
         for point in self.bezier[1:]:
             line(prev_point, point, 'yellow')
             prev_point = point
 
+class Button():
+    def __init__(self, center_pos, txt, w = 150, h = 50, bg = 'black', antialias = True, txt_color = 'white'):
+        self.pos = center_pos
+        self.w = w
+        self.h = h
+        self.txt = txt
+        self.antialias = antialias
+        self.txt_color = txt_color
+        self.bg = bg
+    
+    def render(self):
+        self.bg_rect = pygame.Rect((self.pos[0]-(self.w/2)), (self.pos[1]-(self.h/2)), self.w, self.h)
+        self.text = font.render(self.txt, self.antialias, self.txt_color)
+        self.rect = self.text.get_rect(center = self.pos)
+        pygame.draw.rect(screen, self.bg, self.bg_rect)
+        screen.blit(self.text, self.rect)
+
+    def pressed(self):
+        mouse_pos = pygame.mouse.get_pos()
+        self.min_x = self.pos[0]-(self.w/2)
+        self.max_x = self.pos[0]+(self.w/2)
+        self.min_y = self.pos[1]-(self.h/2)
+        self.max_y = self.pos[1]+(self.h/2)
+        return self.min_x < mouse_pos[0] < self.max_x and self.min_y < mouse_pos[1] < self.max_y
+
+class Text():
+    def __init__(self, center_pos, txt, size, color = 'white', bg_color = 'black', antialias = True, font = 'arial'):
+        self.txt = txt
+        self.color = color
+        self.bg_color = bg_color
+        self.antialias = antialias
+        self.center_pos = center_pos
+        self.font = pygame.font.SysFont(font, size)
+        self.rendered_txt = self.font.render(self.txt, self.antialias, self.color, self.bg_color)
+        self.rect = self.rendered_txt.get_rect(center = self.center_pos)
+
+    def render(self):
+        screen.blit(self.rendered_txt, self.rect)
+
+    def update(self, new_txt = None, new_center = None, new_color = None, new_bg_color = None):
+        if new_txt == None:
+            new_txt = self.txt
+        else:
+            self.txt = new_txt
+
+        if new_center == None:
+            new_center = self.center_pos
+        else:
+            self.center_pos = new_center
+
+        if new_color == None:
+            new_color = self.color
+        else:
+            self.color = new_color
+
+        if new_bg_color == None:
+            new_bg_color = self.bg_color
+        else:
+            self.bg_color = new_bg_color
+
+        self.rendered_txt = self.font.render(self.txt, self.antialias, self.color, self.bg_color)
+        self.rect = self.rendered_txt.get_rect(center = self.center_pos)
+
+    
+
 
 points_rect = list()
+all_p = list()
+quadratic_points = list()
+cubic_points = list()
+quartic_points = list()
 
 curve = Curve()
 
+text_offsett = Vector2(-10, -10)
+
 start_point = curve.p0
-points_rect.append([start_point, create_dot_rect_surface(start_point), False])
+text_pos = start_point+text_offsett
+all_p.append([start_point, create_dot_rect_surface(start_point), False, Text((text_pos.x, text_pos.y), 'P', 10)])
 
 curving_point = Vector2(100, 400)
-points_rect.append([curving_point, create_dot_rect_surface(curving_point), False])
+text_pos = curving_point+text_offsett
+all_p.append([curving_point, create_dot_rect_surface(curving_point), False, Text((text_pos.x, text_pos.y), 'P', 10)])
 
 curving_point2 = Vector2(200, 50)
-points_rect.append([curving_point2, create_dot_rect_surface(curving_point2), False])
+text_pos = curving_point2+text_offsett
+all_p.append([curving_point2, create_dot_rect_surface(curving_point2), False, Text((text_pos.x, text_pos.y), 'P', 10)])
 
 curving_point3 = Vector2(500, 150)
-points_rect.append([curving_point3, create_dot_rect_surface(curving_point3), False])
+text_pos = curving_point3+text_offsett
+all_p.append([curving_point3, create_dot_rect_surface(curving_point3), False, Text((text_pos.x, text_pos.y), 'P', 10)])
 
 end_point = curve.p1
-points_rect.append([end_point, create_dot_rect_surface(end_point), False])
+text_pos = end_point+text_offsett
+all_p.append([end_point, create_dot_rect_surface(end_point), False, Text((text_pos.x, text_pos.y), 'P', 10)])
 
-print(points_rect)
+for idx, point in enumerate(all_p[:2].copy()):
+    point[3].update(new_txt = (f'P{idx}'))
+    quadratic_points.append(point)
+last_p = all_p[-1].copy()
+last_p[3].update(new_txt = f'P{len(quadratic_points)}')
+quadratic_points.append(last_p)
+
+for p in quadratic_points:
+    print(p[3].txt)
+
+for idx, point in enumerate(all_p[:3].copy()):
+    point[3].update(new_txt = (f'P{idx}'))
+    cubic_points.append(point)
+last_p = all_p[-1].copy()
+last_p[3].update(new_txt = f'P{len(cubic_points)}')
+cubic_points.append(last_p)
+
+for idx, point in enumerate(all_p[:4].copy()):
+    point[3].update(new_txt = (f'P{idx}'))
+    quartic_points.append(point)
+last_p = all_p[-1].copy()
+last_p[3].update(new_txt = f'P{len(quartic_points)}')
+quartic_points.append(last_p)
 
 render = False
 moving = False
+mouse_pressed = False
+
+menu = True
+quadratic = False
+quartic = False
+cubic = False
+
+quad_btn = Button((100, 250), 'Quadratic', 160, 50)
+cub_btn = Button((300, 250), 'Cubic', 160, 50)
+quar_btn = Button((500, 250), 'Quartic', 160, 50)
+
+title = Text((WIDTH/2, 75), 'Bezier Curve', 50, bg_color = None)
+description_l1 = Text((WIDTH/2, 470), "Choose the curve that you'd like to see", 24, bg_color=None)
+description_l2 = Text((WIDTH/2, 500), "once you saw it press ESC to come back here", 24, bg_color=None)
 
 while True:
 
@@ -132,41 +251,98 @@ while True:
                     curve.t += 0.001
 
         if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_RETURN:
+            if event.key == pygame.K_RETURN and not moving:
                 render = True
+            elif event.key == pygame.K_ESCAPE:
+                quadratic = False
+                cubic = False
+                quartic = False
+                render = False
+                menu = True
+
+                points_rect = list()
+
+                curve.reset()
 
         if event.type == pygame.MOUSEBUTTONUP:
+            mouse_pressed = False
             moving = False
-
             for p_r in points_rect:
                 p_r[2] = False
 
         elif event.type == pygame.MOUSEBUTTONDOWN:
-            moving = True
-            for p_r in points_rect:
+            mouse_pressed = True
+            if not render:
+                moving = True
+                for p_r in points_rect:
 
-                if p_r[1].collidepoint(event.pos):
-                    p_r[2] = True
-                    break
-                else:
-                    p_r[2] = False
-
-        
+                    if p_r[1].collidepoint(event.pos):
+                        p_r[2] = True
+                        break
+                    else:
+                        p_r[2] = False
 
     screen.fill((25, 25, 25))
-
     mouse_pos = pygame.mouse.get_pos()
 
     for p_r in points_rect:
         if p_r[2] and moving:
             p_r[0] = Vector2(mouse_pos)
+            new_txt_pos = p_r[0] + text_offsett
             p_r[1] = create_dot_rect_surface(p_r[0])
+            p_r[3].update(new_center = (new_txt_pos.x, new_txt_pos.y))
 
         rect(p_r[1])
+        p_r[3].render()
 
-    if render:
-        curve.quartic(points_rect[0][0], points_rect[1][0], points_rect[2][0], points_rect[3][0], points_rect[4][0])
-        curve.render_bezier()
+    if quadratic:
+        if render:
+            curve.quadratic(points_rect[0][0], points_rect[1][0], points_rect[2][0])
+            curve.render_bezier((points_rect[0][0].x, points_rect[0][0].y))
+
+
+    elif cubic:
+        if render:
+            curve.cubic(points_rect[0][0], points_rect[1][0], points_rect[2][0], points_rect[3][0])
+            curve.render_bezier((points_rect[0][0].x, points_rect[0][0].y))
+
+    elif quartic:
+        if render:
+            curve.quartic(points_rect[0][0], points_rect[1][0], points_rect[2][0], points_rect[3][0], points_rect[4][0])
+            curve.render_bezier((points_rect[0][0].x, points_rect[0][0].y))
+
+    elif menu:
+
+        title.render()
+        description_l1.render()
+        description_l2.render()
+
+        quad_btn.render()
+        cub_btn.render()
+        quar_btn.render()
+
+        if mouse_pressed:
+
+            if quad_btn.pressed():
+                menu = False
+                quadratic = True
+                points_rect = quadratic_points.copy()
+                for p in points_rect:
+                    print(p[3].txt)
+
+            elif cub_btn.pressed():
+                menu = False
+                cubic = True
+                points_rect = cubic_points.copy()
+                for p in points_rect:
+                    print(p[3].txt)
+
+            elif quar_btn.pressed():
+                menu = False
+                quartic = True
+                points_rect = quartic_points.copy()
+                for p in points_rect:
+                    print(p[3].txt)
 
 
     pygame.display.flip()
